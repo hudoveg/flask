9f739aba30475dbb5340d16d37bec7a55c2a5be2
Huu <huu.dm@teko.vn>
2019-04-11 16:31:13 +0700

    correct spelling

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,6 @@ db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     from .views import auth_blueprint, author_blueprint, user_blueprint
